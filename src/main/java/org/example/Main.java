e7b667b113bac25d9ce70a306e19d1a50bf55d94
EvErToN <evertonflorenciodasilva@outlook.com>
2024-12-25 17:11:38 -0300

    criei a logica inicial para converter os numeros em um float só falta a string

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,20 @@ package org.example;
 
 
 import javax.swing.*;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Scanner;
 
 public class Main {
 
 
     public static void main(String[] args) {
-
-    JOptionPane.showMessageDialog(null,"escreva o numero a ser convertido");
-
-
+        System.out.println( "digite o numero a ser convertido : " );
+        //entrada de dados//
+        Scanner scan = new Scanner(System.in);
+        String numerodigitado = scan.nextLine();
+        //converção//
+        float numero = Float.parseFloat(numerodigitado);
+        System.out.println( "numero : " + numero + " convertido com sucesso" );
     }
 }
